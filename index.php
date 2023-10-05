ac4e730572281d056ab0760f64efdf9c565f64f9
HenriqueDSCarvalho <h96352828@gmail.com>
2023-10-05 15:15:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div class="login">
+        <div class="container">
+            <div class="row col-lg-4 offset-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-body">
+                            <h3>Login</h3>
+                        </div>
+                        <form action="login.php" method="POST">
+                            <div>
+                                <div class="mb-3">
+                                    <label>Usuário</label>
+                                    <input type="text" name="Usuario" class="form-control">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="mb-3">
+                                    <label>Senha</label>
+                                    <input type="password" name="senha" class="form-control">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-primary">enviar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
